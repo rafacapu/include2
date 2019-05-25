f36fbf24864758a02e7c19eefd8d67e1927dd2b7
rafacapu <rafacapuano@gmail.com>
2019-05-25 01:38:52 -0300

    finalizar

---
+++ MenuPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using App_Secao07.Views;
+using App_Secao07.Pages;
+using App_Secao07.ViewModels;
+
+namespace App_Secao07.Views
+{
+    public partial class MenuPage : MasterDetailPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+        private void GoPaginaXamarin( object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new XamarinPage());
+        }
+        private void GoPaginaPerfil(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Perfil());
+        }
+    }
+}
---
+++ Perfil.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using System;
+using Xamarin.Forms;
+using App_Secao07.Views;
+using App_Secao07.Pages;
+using App_Secao07.ViewModels;
+
+namespace App_Secao07.Views
+{
+    public partial class Perfil : ContentPage
+    {
+        public Perfil()
+        {
+            InitializeComponent();
+        }
+    }
+}
